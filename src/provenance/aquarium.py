2b0b3af047e23cf42390844ab1ed60e841c0f895
Ben Keller <bjkeller@uw.edu>
2018-08-13 14:30:24 -0700

    Fix handling of parts and operations
    1. Fix handling of sources for collection parts.
    2. Add operation attributes

--- src/provenance/aquarium.py
+++ src/provenance/aquarium.py
@@ -11,8 +11,9 @@ a collection has no sample but has an object_type; and
 a part of a collection has a sample but no object_type.
 """
 import abc
-import json
+import re
 import sys
+from collections.abc import Mapping
 
 
 class AttributesMixin(abc.ABC):
@@ -93,6 +94,9 @@ class AbstractItemEntity(AbstractEntity, AttributesMixin):
         attr_dict = AttributesMixin.as_dict(self)
         return {**item_dict, **{**entity_dict, **attr_dict}}
 
+    def is_collection(self):
+        return False
+
 
 class ItemEntity(AbstractItemEntity):
     """
@@ -120,6 +124,9 @@ class ItemEntity(AbstractItemEntity):
         item_dict['object_type'] = type_dict
         return item_dict
 
+    def get_sample(self):
+        return self.sample
+
 
 class CollectionEntity(AbstractItemEntity):
     """
@@ -141,6 +148,9 @@ class CollectionEntity(AbstractItemEntity):
         type_dict['object_type_name'] = self.object_type.name
         item_dict['object_type'] = type_dict
         return item_dict
+
+    def is_collection(self):
+        return True
 
 
 class FileEntity(AbstractEntity):
@@ -174,6 +184,7 @@ class OperationActivity(AttributesMixin):
         self.operation_type = operation.operation_type
         self.operation = operation
         self.inputs = list()
+        super().__init__()
 
     def add_input(self, input):
         self.inputs.append(input)
@@ -190,12 +201,13 @@ class OperationActivity(AttributesMixin):
         op_type['name'] = self.operation_type.name
         op_dict['operation_type'] = op_type
         op_dict['inputs'] = [input.as_dict() for input in self.inputs]
-        return op_dict
+        attr_dict = AttributesMixin.as_dict(self)
+        return {**op_dict, **attr_dict}
 
 
 class OperationArgument(abc.ABC):
     """
-    Models an argument to an operation, which can be either a 
+    Models an argument to an operation, which can be either a
     (though use it to capture output during trace conversion)
     """
 
@@ -336,6 +348,7 @@ class PlanTrace(AttributesMixin):
         return {**trace_dict, **super_dict}
 
 
+# TODO: ensure that getting sources for all items
 class TraceFactory:
     """
     Defines a factory object to create a PlanTrace from a pydent.model.Plan.
@@ -367,7 +380,7 @@ class TraceFactory:
 
         return factory.trace
 
-    def _add_item_entity(self, *, entity, generator):
+    def _add_item_entity(self, *, entity, generator=None):
         if generator:
             entity.add_generator(generator)
         self.trace.add_item(entity)
@@ -405,6 +418,14 @@ class TraceFactory:
                             item_id=arg.item_id,
                             generator=op_activity
                         )
+
+        if operation.data_associations:
+            for association in operation.data_associations:
+                if association.upload:
+                    msg = "WARNING: ignoring upload for operation {}"
+                    print(msg.format(op_activity.operation_id))
+                elif association.object:
+                    op_activity.add_attribute(association.object)
 
     def _create_items(self, *, item_id, generator=None):
         item_obj = self.session.Item.find(item_id)
@@ -462,64 +483,176 @@ class TraceFactory:
         for i in range(len(collection.matrix)):
             row = collection.matrix[i]
             for j in range(len(row)):
-                if row[j] and not row[j] < 0:
-                    part_id = str(item_id) + '/' + chr(ord('A')+i) + str(j+1)
-                    sample = self.session.Sample.find(row[j])
-                    if sample:
-                        part_entity = PartEntity(
-                            part_id=part_id,
-                            sample=sample,
-                            collection=entity
-                        )
-                        self._add_item_entity(
-                            entity=part_entity,
-                            generator=generator
-                        )
+                sample = self._get_sample(row[j])
+                source_id = TraceFactory._get_source_id(routing_matrix, i, j)
 
-                        # Add part as source to file
-                        if upload_matrix:
-                            upload_id = upload_matrix[i][j]
-                            self._get_file(
-                                upload_id=upload_id,
-                                source=part_entity
-                            )
-                        # Deal with routing
-                        if routing_matrix:
-                            entry = routing_matrix[i][j]
-                            if entry:
-                                source_id = entry['source']
-                                if not self.trace.has_item(source_id):
-                                    print("no item for {}".format(source_id),
-                                          file=sys.stderr)
-                                else:
-                                    item_entity = self.trace.get_item(
-                                        source_id)
-                                    part_entity.add_source(item_entity)
-                                    
-                                if 'attributes' in entry:
-                                    attributes = entry['attributes']
-                                    part_entity.add_attribute(attributes)
+                # has to be either a sample or source_id
+                if not sample and not source_id:
+                    continue
+
+                source_entity = None
+                if source_id:
+                    source_entity = self._get_source(source_id)
+                    if not sample:
+                        sample = source_entity.get_sample()
+                        # TODO: decide whether to flag inconsistency
+
+                part_id = str(item_id) + '/' + well_coordinates(i, j)
+                part_entity = PartEntity(
+                    part_id=part_id,
+                    sample=sample,
+                    collection=entity
+                )
+                self._add_item_entity(entity=part_entity, generator=generator)
+                if source_entity:
+                    part_entity.add_source(source_entity)
+
+                # Add part as source to file linked in upload_matrix
+                if upload_matrix:
+                    upload_id = upload_matrix[i][j]
+                    self._get_file(upload_id=upload_id, source=part_entity)
+
+                attributes = TraceFactory._get_attributes(routing_matrix, i, j)
+                part_entity.add_attribute(attributes)
+
+    def _get_source(self, source_id):
+        """
+        Returns an entity for the ID, creating the entity if it does not
+        already exist.
+
+        Source IDs come from data associations of collections and indicate the
+        source for a part.
+
+        May have one of the forms
+        - item_id
+        - item_id/part_ref
+        - object_type_name/item_id/sample_id/part_ref
+        The latter form is used in cases where the item is not a collection,
+        but consists of subparts that are not explicitly modeled.
+        An example is a yeast plate with colonies.
+        In this case, return the item.
+
+        Some plans have a part_ref of the form [[i,j]] that needs to be
+        converted to alphanumeric form.
+
+        This should not be necessary once part are first order in aquarium.
+        """
+        if self.trace.has_item(source_id):
+            return self.trace.get_item(source_id)
+
+        source_components = source_id.split('/')
+        if re.match("[0-9]+", source_id):
+            source_item_id = source_components[0]
+            part_ref = None
+            if len(source_components) == 2:
+                part_ref = source_components[1]
+                # fix stray numeric coordinates
+                pattern = r"\[\[([0-9]+),[ \t]*([0-9]+)\]\]"
+                match = re.match(pattern, part_ref)
+                if match:
+                    part_ref = well_coordinates(
+                        int(match[1]), int(match[2]))
+                # TODO: handle bad part ref
+        elif len(source_components) == 4:
+            # TODO: check this is an identifier
+            source_item_id = source_components[1]
+            part_ref = source_components[3]
+        else:
+            # TODO: raise exception here since id is malformed
+            msg = "WARNING: unrecognized source ID: {}"
+            print(msg.format(source_id), file=sys.stderr)
+            return None
+
+        if self.trace.has_item(source_item_id):
+            source_item_entity = self.trace.get_item(source_item_id)
+        else:
+            item_obj = self.session.Item.find(source_item_id)
+            if is_collection(item_obj):
+                item_obj = self.session.Collection.find(source_item_id)
+                source_item_entity = CollectionEntity(collection=item_obj)
+            else:
+                source_item_entity = ItemEntity(item=item_obj)
+            self._add_item_entity(entity=source_item_entity)
+
+        if not part_ref:
+            return source_item_entity
+
+        if not source_item_entity.is_collection():
+            msg = "WARNING: ignoring part {} from non-collection {} in source"
+            print(msg.format(part_ref, source_item_id))
+            return source_item_entity
+
+        # this assumes part_ref is well-formed
+        (i, j) = TraceFactory.split_well_coordinate(part_ref)
+        sample_id = source_item_entity.collection.matrix[i][j]
+        sample = self.session.Sample.find(sample_id)
+
+        source_part_entity = PartEntity(
+            part_id=source_item_id + '/' + part_ref,
+            sample=sample,
+            collection=source_item_entity
+        )
+        self._add_item_entity(entity=source_part_entity)
+        return source_part_entity
+
+    @staticmethod
+    def split_well_coordinate(part_ref):
+        pattern = r"([A-Z])([0-9]+)"
+        match = re.match(pattern, part_ref)
+        if match:
+            return (ord('A') - ord(match[1]), int(match[2]))
+        pattern = r"\[\[([0-9]+),[ \t]*([0-9]+)\]\]"
+        match = re.match(pattern, part_ref)
+        if match:
+            return (int(match[1]), int(match[2]))
 
     def _get_file(self, *, upload_id, source=None):
         """
         Returns the file entity for an upload associated with a plan.
         If the entity is not currently in the trace, creates it.
         """
-        if self.trace.has_file(upload_id):
-            if source:
-                file_entity = self.trace.get_file(upload_id)
-                file_entity.add_source(source)
-        else:
+        if not self.trace.has_file(upload_id):
             file_entity = FileEntity(
                 upload=self._get_upload(upload_id)
             )
             self._add_file_entity(entity=file_entity, source=source)
+        else:
+            file_entity = self.trace.get_file(upload_id)
+        if source:
+            file_entity.add_source(source)
 
     def _get_upload(self, upload_id):
         return self.session.Upload.where(
             {"id": upload_id},
             {"methods": ["size", "name"]}
         )[0]
+
+    def _get_sample(self, sample_id: int):
+        if sample_id and not sample_id < 0:
+            return self.session.Sample.find(sample_id)
+
+    @staticmethod
+    def _get_attributes(routing_matrix, i, j):
+        entry = TraceFactory._get_routing_entry(routing_matrix, i, j)
+        if 'attributes' not in entry:
+            return dict()
+        return entry['attributes']
+
+    @staticmethod
+    def _get_source_id(routing_matrix, i, j):
+        entry = TraceFactory._get_routing_entry(routing_matrix, i, j)
+        if 'source' in entry:
+            return str(entry['source'])
+        return None
+
+    @staticmethod
+    def _get_routing_entry(routing_matrix, i, j):
+        if not routing_matrix:
+            return dict()
+        entry = routing_matrix[i][j]
+        if entry and isinstance(entry, Mapping):
+            return entry
+        return dict()
 
 
 def is_item_field_value(field_value):
@@ -552,3 +685,7 @@ def is_routing_matrix(association):
 
 def get_routing_matrix(association_object):
     return association_object['routing_matrix']['rows']
+
+
+def well_coordinates(i: int, j: int):
+    return chr(ord('A')+i) + str(j+1)
