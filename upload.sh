6a022c4c5a51c6781bcdac0d0fef6c4cc919d721
Ben Keller <bjkeller@uw.edu>
2018-09-15 10:54:56 -0700

    Add script to upload yg and nc runs

---
+++ upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ARGS='-u --dump dump -v'
+#./trace.sh -p 14866 -c yg $ARGS
+#./trace.sh -p 15984 -c yg $ARGS
+#./trace.sh -p 17026 -c yg $ARGS
+#./trace.sh -p 17413 -c yg $ARGS
+#./trace.sh -p 17987 -c yg $ARGS
+#./trace.sh -p 18527 -c yg $ARGS
+#./trace.sh -p 18598 -c yg $ARGS
+#./trace.sh -p 14866 -c yg $ARGS
+./trace.sh -p 15901 -c nc $ARGS
+#./trace.sh -p 16122 -c nc $ARGS
+#./trace.sh -p 17016 -c nc $ARGS
