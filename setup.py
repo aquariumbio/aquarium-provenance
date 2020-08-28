52e338175682521fa32a9612aacabefc16c6f063
Ben Keller <bjkeller@uw.edu>
2020-08-28 11:29:41 -0700

    Update setup

--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='aquarium-provenance',
+    version='1.0.0',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=[
-        'pydent==0.0.35',
         'boto3',
         'pySBOL'
-    ]
+    ],
+
+    author="Ben Keller",
+    author_email="bjkeller@uw.edu",
+    description="Package for capturing PROV-like provenance of files and items in Aquarium",
+    url="https://aquariumbio.bio",
+    project_urs={
+        "Source Code": "https://github.com/aquariumbio/aquarium-provenance"
+    }
+
 )
