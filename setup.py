8deb2b2d71b60c0874151997a0ee6688084e3e27
Ben Keller <bjkeller@uw.edu>
2020-08-28 11:46:19 -0700

    Fix setup file and update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aquarium-provenance',
-    version='1.0.0',
+    version='1.0.1',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=[
@@ -14,7 +14,7 @@ setup(
     author_email="bjkeller@uw.edu",
     description="Package for capturing PROV-like provenance of files and items in Aquarium",
     url="https://aquariumbio.bio",
-    project_urs={
+    project_urls={
         "Source Code": "https://github.com/aquariumbio/aquarium-provenance"
     }
 
